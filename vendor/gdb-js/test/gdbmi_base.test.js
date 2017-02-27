5db477083b517c7d72c2ae33eb4c4cafb901adb8
Sheng Chen <t-sheche@microsoft.com>
2017-02-27 14:00:06 +0800

    Cs testcase init (#84)
    * initial version for wrapping gdb in node.js, run gulp to get babled code.
    
    * init test cases
    
    * devide testcases
    
    * make the config file similar with launch.json as vs code

---
+++ vendor/gdb-js/test/gdbmi_base.test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import os from 'os';
+
+import NodeGDB from '../src/node-gdbmi';
+import config from './config.json';
+
+let GDB_LOCATION = config.gdbLocation[os.platform()];
+
+describe('GDB base', () => {
+
+  let GDB = null;
+
+  before((done) => {
+    GDB = new NodeGDB({
+      command: GDB_LOCATION
+    });
+    done();
+  });
+
+  it('should be able to connect', async() => {
+    const ret = await GDB.start();
+    expect(ret.state).to.equal('done');
+  });
+
+  it('should be able to stop', async() => {
+    const ret = await GDB.stop();
+    expect(ret.state).to.equal('exit');
+  });
+
+  after((done) => {
+    GDB = null;
+    done();
+  });
+});
